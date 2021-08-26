bc8679dc253d4177e719c395f50d3538d4ed1df4
Evans Ding <evansding@gmail.com>
2021-08-26 00:56:25 -0700

    setup github api

---
+++ src/Github.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { githubKey } from './config/config.js';
+
+
+const Github = (props) => {
+
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    const option = {
+      url: 'https://api.github.com/users/evansding123/repos',
+      headers: {
+        'User-Agent': 'request',
+        Authorization: `${githubKey}`,
+      }
+    }
+    const getRepo = async () => {
+      try {
+        const response = await axios(option);
+
+        response.data.sort((a, b) => {
+          return (a.id - b.id);
+        }).reverse();
+        console.log(response.data);
+        setRepos(response.data);
+      } catch(error) {
+        console.log(error);
+      }
+    }
+
+    getRepo();
+  }, [])
+
+
+
+
+  return(
+    <div>{repos.map((item, index) => {
+      if(index < 5) {
+        return(
+          <div>{item.full_name}</div>
+        );
+      }
+    })}</div>
+  )
+}
+
+export default Github;
---
+++ src/config/config.js
@@ -0,0 +1,2 @@
+export const githubKey = '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';
+
